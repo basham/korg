0d9b39f0aabb085ffeb176e1310285468ef1186c
Chris Basham <chris@bash.am>
2023-03-31 23:08:27 -0400

    Work on locations

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -3,6 +3,8 @@
 	import Layout from './layout.svelte';
 
 	const NEW_GAME = Symbol('new game');
+	const TRAVEL = Symbol('travel');
+	const TRAVEL_TO_LOCATION = Symbol('travel to location');
 	const ENCOUNTER_FOE = Symbol('encounter foe');
 	const ENCOUNTER_TRAP = Symbol('encounter trap');
 	const DEFEAT_FOE = Symbol('defeat foe');
@@ -12,19 +14,40 @@
 	const GOLD = 0;
 	const HEALTH = 20;
 
-	const location = {
-		label: 'Ruin',
+	const ruinLocation = {
+		label: 'Ruins',
 		encounters: [
-			['Spike trap', null, 3, 0],
+			['Pit trap', null, 3, 0],
 			['Skeleton', 2, 2, 1],
 			['Goblin', 3, 2, 2],
 			['Wraith', 5, 2, 4],
 			['Ogre', 4, 4, 7],
 			['Demon', 4, 6, 10]
-		].map(([label, defense, damage, gold]) =>
+		]
+	};
+
+	const catacombsLocation = {
+		label: 'Catacombs',
+		encounters: [
+			['Spike trap', null, 4, 'd6'],
+			['Zombie', 2, 2, 1],
+			['Ghoul', 2, 3, 2],
+			['Ghost', 4, 2, 4],
+			['Banshee', 3, 6, 7],
+			['Lich', 5, 4, 10]
+		]
+	};
+
+	const locations = [
+		ruinLocation,
+		catacombsLocation
+	].map((location) => {
+		const id = Symbol(location.label);
+		const encounters = location.encounters.map(([label, defense, damage, gold]) =>
 			({ label, defense, damage, gold, trap: defense === null })
-		)
-	}
+		);
+		return { ...location, id, encounters };
+	});
 
 	let log = [];
 	$: event = log.at(-1) || {};
@@ -37,14 +60,25 @@
 		log = [];
 		pushEvent(NEW_GAME, {
 			gold: GOLD,
-			health: HEALTH,
-			location
+			health: HEALTH
+		});
+	}
+
+	function travel () {
+		pushEvent(TRAVEL);
+	}
+
+	function travelToLocation (id) {
+		pushEvent(TRAVEL_TO_LOCATION, {
+			location: id,
+			getLocation: () => locations.find((location) => location.id === id)
 		});
 	}
 
 	function explore () {
-		const result = roll();
-		const encounter = location.encounters[result - 1];
+		const { encounters } = event.getLocation();
+		const result = roll(encounters.length);
+		const encounter = encounters[result - 1];
 		const type = encounter.trap ? ENCOUNTER_TRAP : ENCOUNTER_FOE;
 		pushEvent(type, {
 			encounter,
@@ -53,10 +87,12 @@
 	}
 
 	function encounterTrap () {
-		const { damage } = event.encounter;
+		const { damage, gold } = event.encounter;
+		const result = gold === 'd6' ? roll() : 0;
 		pushEvent(TAKE_DAMAGE_FROM_TRAP, {
+			gold: event.gold + result,
 			health: event.health - damage,
-			result: null
+			result
 		});
 	}
 
@@ -107,22 +143,33 @@
 				map.set(label, map.get(label) + 1);
 				return map;
 			}, new Map());
-		if (!foes.size) {
-			return '(none)';
-		}
 		return [...foes.entries()]
-			.map(([label, count]) => `${label} (${count})`)
-			.join(', ');
+			.map(([label, count]) => `${label} (${count})`);
 	}
 </script>
 
 <Layout>
 	<h1>Korg</h1>
 	<p class="status">{event.health} health<br>{event.gold} gold</p>
-	<p>Defeated foes: {defeatedFoes(log)}</p>
+	{#if event.location}
+		<p>Location: {event.getLocation().label || '(none)'}</p>
+	{/if}
+	{#if defeatedFoes(log).length}
+		<p>Defeated foes: {defeatedFoes(log).join(', ')}</p>
+	{/if}
 	<article aria-label="Current event" class="event" id="current-event" tabindex="-1">
 		{#if event.type === NEW_GAME}
 			<p>You are an adventurer.</p>
+			<p><button on:click={travel}>Start traveling</button></p>
+		{/if}
+		{#if event.type === TRAVEL}
+			<p>Pick a location.</p>
+			{#each locations as location}
+				<p><button on:click={() => travelToLocation(location.id)}>{location.label}</button></p>
+			{/each}
+		{/if}
+		{#if event.type === TRAVEL_TO_LOCATION}
+			<p>You enter the {event.getLocation().label}.</p>
 			<p><button on:click={explore}>Explore</button></p>
 		{/if}
 		{#if event.type === ENCOUNTER_FOE}
@@ -150,6 +197,9 @@
 		{/if}
 		{#if event.type === TAKE_DAMAGE_FROM_TRAP}
 			<p>Take <strong>{event.encounter.damage} damage</strong> from the <strong>{event.encounter.label}</strong>.</p>
+			{#if event.encounter.gold === 'd6'}
+				<p>You discover <strong>{event.result} gold</strong>.</p>
+			{/if}
 			{#if event.health <= 0}
 				<p>You die.</p>
 				<p><button on:click={newGame}>New game</button></p>
